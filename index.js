a901774be3e58b18e0042bbc83d5c2e2676419ff
Michelle Vitello <michelleramona89@gmail.com>
2022-09-15 07:41:17 -0400

    commit tree 11b6d66388bb884f9deb200800eaba7fa392e638

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+//const cards = (["Charlie", "Samip", "Ali"], "birthday");
+
+function writeCards(names, event) {
+    const thankYouCards = []
+    for (let i = 0; i < names.length; i++) {
+    thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);}
+
+return thankYouCards;
+}
+
+function countDown(){
+    let countUp = 0;
+    while (countUp<10){
+        console.log(countUp++);
+    }
+}
